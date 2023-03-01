9ca73256977c1b1056dbdd70f1f8ac616d6fc687
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-03-01 17:13:42 +0300

    feat: add project descriptions and buttons to the landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import Button from "../Button/Button";
 import "./Landing.css";
 
 const Landing = () => {
+  const history = useHistory();
+  const redirectTo = (path) => {
+    history.push(path);
+  };
+
   return (
     <div className="landing-wrapper">
       <div className="landing-container">
@@ -11,9 +18,55 @@ const Landing = () => {
           </div>
           <div className="landing-divider"></div>
           <div className="landing-lower-area">
-            <div>ML Interface Wizard</div>
-            <div>ML Measure</div>
-            <div>ML Contribute</div>
+            <div className="project-container">
+              <h2 className="project-header">ML Interface Wizard</h2>
+              <p className="project-description">
+                An application that generates front-ends to any input machine
+                learning models.
+                {/*TODO: improve the description*/}
+              </p>
+              <Button
+                type="submit"
+                onClick={() => {
+                  redirectTo("/interface-wizard");
+                  window.scrollTo(0, 0);
+                }}
+                buttonType="success"
+                text="Go to the Project 🚀"
+              />
+            </div>
+            <div className="project-container">
+              <h2 className="project-header">ML Measure</h2>
+              <p>
+                Project Description
+                {/*TODO: write description for ML Measure*/}
+              </p>
+              <Button
+                type="submit"
+                onClick={() => {
+                  redirectTo("/measure");
+                  window.scrollTo(0, 0);
+                }}
+                buttonType="success"
+                text="Go to the Project 🚀"
+              />
+            </div>
+            <div className="project-container">
+              <h2 className="project-header">ML Contribute</h2>
+              <p>
+                Project Description
+                {/*TODO: write description for ML Contribute*/}
+              </p>
+              <Button
+                type="submit"
+                onClick={() => {
+                  redirectTo("/contribute");
+                  window.scrollTo(0, 0);
+                }}
+                buttonType="success"
+                text="Go to the Project 🚀"
+              />
+            </div>
           </div>
         </div>
       </div>
