0d7032eda1d7581f069c4c26db23ef9828456268
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-03-19 19:21:19 +0300

    feat: custom style for the Dropdown component

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,8 +1,30 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
+import { alpha, styled } from "@mui/material/styles";
+import { pink } from "@mui/material/colors";
 
-const Dropdown = ({ options, onChange, value, disabled=false }) => {
+const Dropdown = ({ options, onChange, value, disabled = false }) => {
+  const StyledTextField = styled(TextField)({
+    "& label.Mui-focused": {
+      color: "#ad1457",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "#ad1457",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "#ad1457",
+      },
+      "&:hover fieldset": {
+        borderColor: "#ad1457",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "#ad1457",
+      },
+    },
+  });
+
   return (
     <Autocomplete
       disablePortal
@@ -11,8 +33,10 @@ const Dropdown = ({ options, onChange, value, disabled=false }) => {
       id="combo-box-demo"
       options={options}
       value={value}
-      sx={{ width: 300, color: "#e91e63" }}
-      renderInput={(params) => <TextField {...params} label="Movie" />}
+      sx={{
+        width: 300,
+      }}
+      renderInput={(params) => <StyledTextField {...params} label="Movie" />}
     />
   );
 };
