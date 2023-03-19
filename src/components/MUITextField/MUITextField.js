a899b06e23794a0a68cc2def7bd00ea87389b1bc
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-03-19 21:09:09 +0300

    feat: create a text field element

---
+++ src/components/MUITextField/MUITextField.js
@@ -0,0 +1,45 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import { styled } from "@mui/material/styles";
+
+const MUITextField = ({
+  label,
+  helperText,
+  defaultValue,
+  name,
+  onChange,
+  inputProps,
+}) => {
+  const StyledTextField = styled(TextField)({
+    "& label.Mui-focused": {
+      color: "#ad1457",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "#ad1457",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "#ad1457",
+      },
+      "&:hover fieldset": {
+        borderColor: "#ad1457",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "#ad1457",
+      },
+    },
+  });
+  return (
+    <StyledTextField
+      label={label}
+      helperText={helperText}
+      defaultValue={defaultValue}
+      type="number"
+      name={name}
+      onChange={onChange}
+      inputProps={inputProps}
+    ></StyledTextField>
+  );
+};
+
+export default MUITextField;
