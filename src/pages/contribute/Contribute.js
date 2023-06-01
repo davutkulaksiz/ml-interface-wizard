332ab1360e02f5a92427f9861db4a6810b3116e6
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-06-01 09:37:59 +0300

    fix(ml-tools): replace sidebar with drawer

--- src/pages/contribute/Contribute.js
+++ src/pages/contribute/Contribute.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import Sidebar from "../../components/Sidebar/Sidebar";
 import WIPcontribute from "../../assets/WIPcontribute.gif";
 import "./Contribute.css";
 
@@ -9,7 +8,6 @@ const Contribute = () => {
     <>
       <Navbar />
       <div className="contribute-container">
-        <Sidebar />
         <div className="work-in-progress">
           <img className="work-in-progress-gif" src={WIPcontribute} alt="Work-in-progress"></img>
         </div>
