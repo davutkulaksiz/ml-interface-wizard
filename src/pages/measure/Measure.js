ca240e96a8dff4ef4363dd117c142328b4798d9c
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-06-01 10:08:46 +0300

    Update some visuals (#14)
    * feat(navbar): add margin to the navbar to make components stand out individually
    
    * feat(landing): add border shadows to the landing page to make it stand out visually
    
    * fix(ml-interface): make cards have the same height
    
    * fix(ml-tools): replace sidebar with drawer
    
    * feat(ml-tools): improve consistency with margins

--- src/pages/measure/Measure.js
+++ src/pages/measure/Measure.js
@@ -1,16 +1,13 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import Sidebar from "../../components/Sidebar/Sidebar";
 import FormAlternative from '../../components/FormAlternative/FormAlternative';
 import "./Measure.css";
-import Button from "../../components/Button/Button";
 
 const Measure = () => {
   return (
     <>
       <Navbar />
       <div className="measure-container">
-        <Sidebar />
         <div className="main-wrapper" > 
         <FormAlternative isTrue={true} formName="Malignant Detection Model" movie="Es Bestas" animals={["Cat", "Dog"]}  />
         </div>
